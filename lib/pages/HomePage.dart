bcaf45425c780c03c0a15aa50a361657a7c9cf37
itsmedona <donashaji254@gmail.com>
2024-05-29 11:14:23 +0530

    first commit

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_portfolio/constants/colors.dart';
+import 'package:my_portfolio/constants/nav_titles.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,10 +13,72 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home"),
-      ),
-
-    );
+        backgroundColor: ColorConstant.scaffoldBg,
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            //MAIN
+            Container(
+              height: 60.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.transparent, ColorConstant.bgLight1]),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Row(
+                children: [
+                  Text(
+                    "DS",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                      color: ColorConstant.yellowSecondary,
+                    ),
+                  ),
+                  Spacer(),
+                  for (int i = 0; i < navTitles.length; i++)
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20.0),
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          navTitles[i],
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: ColorConstant.whitePrimary),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            //SKILLS
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            //PROJECTS
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            ),
+            //CONTACT
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            //FOOTER
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            )
+          ],
+        ));
   }
 }
